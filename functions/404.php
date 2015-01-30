8a983f3376c41d13557a8542a6a29cc607f2b417
Benson Ho <benson@goodafternoon.com.au>
2015-01-30 13:10:21 +0900

    adding base less files

---
+++ functions/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @package WordPress
+ */
+
+ ?>
+<?php get_header(); ?>
+
+<div class="row">
+  <div class="col-sm-12">
+  <div class="post">
+    <h1>Page not Found - 404</h1>
+    <p>
+      Sorry, the page you were looking for does not exist.<br>
+      Please <a href=>contact us</a> if you need some help!
+      <br>
+      <br>
+      <a href="/">Back to Home</a>
+    </p>
+  </div>
+  </div>
+</div>
+
+
+<?php get_footer(); ?>
---
+++ functions/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Default Page
+ *
+ * @package WordPress
+ * @subpackage
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="row">
+</div>
+
+<?php get_footer(); ?>

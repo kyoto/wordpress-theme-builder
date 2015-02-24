4e8ecd4d8647e5914dac1704b1def04a3ec84620
Benson Ho <benson@goodafternoon.com.au>
2015-02-24 16:53:49 +0900

    bugs with coffeescript files

--- assets/javascripts/coffee/index.coffee
+++ assets/javascripts/coffee/index.coffee
@@ -1,7 +1,7 @@
 module.exports = [
-  "./assets/javascripts/js/jquery-1.11.1.min.js"
-  "./assets/javascripts/js/headshrinker.js"
-  "./assets/javascripts/js/scrollup.js"
-  "./assets/javascripts/js/slicknav.js"
+  "./assets/javascripts/js/jquery-1.11.2.js"
+  "./assets/javascripts/js/jquery.headshrinker.js"
+  "./assets/javascripts/js/jquery.scrollup.js"
+  "./assets/javascripts/js/jquery.slicknav.js"
   "./assets/javascripts/js/application.js"
 ]
--- assets/javascripts/js/index.js
+++ assets/javascripts/js/index.js
@@ -1,1 +1,1 @@
-module.exports = ["./assets/javascripts/js/jquery-1.11.1.min.js", "./assets/javascripts/js/headshrinker.js", "./assets/javascripts/js/scrollup.js", "./assets/javascripts/js/slicknav.js", "./assets/javascripts/js/application.js"];
+module.exports = ["./assets/javascripts/js/jquery-1.11.2.js", "./assets/javascripts/js/jquery.headshrinker.js", "./assets/javascripts/js/jquery.scrollup.js", "./assets/javascripts/js/jquery.slicknav.js", "./assets/javascripts/js/jquery.application.js"];

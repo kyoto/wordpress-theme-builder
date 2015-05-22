c9eefd6eb8f9fdd482ea84dee43b24f6b6a52cf4
Benson Ho <benson@goodafternoon.com.au>
2015-05-22 14:27:04 +1000

    ignore index.js

--- assets/javascripts/js/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = ["./assets/javascripts/js/jquery-1.11.2.js", "./assets/javascripts/js/jquery.headshrinker.js", "./assets/javascripts/js/jquery.scrollup.js", "./assets/javascripts/js/jquery.slicknav.js", "./assets/javascripts/js/application.js"];
